3b57b6d517ac57b38b901f5c8351a970cf4a92e0
TarasOrlovskyi <taras.orlovskyi@gmail.com>
2021-05-20 21:36:36 +0300

    Filled ProductService
    1. Filled ProductService

--- src/main/java/com/tnt/onlineshop/service/ProductService.java
+++ src/main/java/com/tnt/onlineshop/service/ProductService.java
@@ -1,4 +1,19 @@
 package com.tnt.onlineshop.service;
 
+import com.tnt.onlineshop.entity.Product;
+
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> findAll();
+
+    Product findById(int id);
+
+    boolean add(Product product);
+
+    boolean update(Product product);
+
+    boolean remove(int id);
+
 }
--- src/main/java/com/tnt/onlineshop/service/impl/DefaultProductService.java
+++ src/main/java/com/tnt/onlineshop/service/impl/DefaultProductService.java
@@ -1,4 +1,58 @@
 package com.tnt.onlineshop.service.impl;
 
-public class DefaultProductService {
+import com.tnt.onlineshop.dao.ProductDao;
+import com.tnt.onlineshop.entity.Product;
+import com.tnt.onlineshop.service.ProductService;
+
+import java.util.List;
+
+public class DefaultProductService implements ProductService {
+
+    private final ProductDao productDao;
+
+    public DefaultProductService(ProductDao productDao){
+        this.productDao = productDao;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productDao.findAll();
+    }
+
+    @Override
+    public Product findById(int id) {
+        if (id > 0){
+            return productDao.findById(id);
+        } else {
+            throw new RuntimeException("Id is 0 or less", new IllegalArgumentException());
+        }
+    }
+
+    @Override
+    public boolean add(Product product) {
+        if (product != null){
+            return productDao.add(product);
+        } else {
+            throw new RuntimeException("Entity Product is null");
+        }
+    }
+
+    @Override
+    public boolean update(Product product) {
+        if (product != null){
+            return productDao.update(product);
+        } else {
+            throw new RuntimeException("Entity Product is null");
+        }
+    }
+
+    @Override
+    public boolean remove(int id) {
+        if (id > 0){
+            return productDao.remove(id);
+        } else {
+            throw new RuntimeException("Id is 0 or less", new IllegalArgumentException());
+        }
+    }
+
 }
