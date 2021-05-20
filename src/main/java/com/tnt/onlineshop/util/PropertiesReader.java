12c20ceb4c417acd2686651f3d2ec3349fd4c226
TarasOrlovskyi <taras.orlovskyi@gmail.com>
2021-05-20 19:07:32 +0300

    Added PropertiesReader to project
    1. Added PropertiesReader to project

--- src/main/java/com/tnt/onlineshop/util/PropertiesReader.java
+++ src/main/java/com/tnt/onlineshop/util/PropertiesReader.java
@@ -1,4 +1,51 @@
 package com.tnt.onlineshop.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Properties;
+
 public class PropertiesReader {
+    private static final String INDEPENDENT_PROPERTIES_FILE = "application.properties";
+    private static final Properties properties = new Properties();
+
+    public PropertiesReader() {
+        String env = System.getenv("env");
+        if (env == null) {
+            try (InputStream inputStream = PropertiesReader.class.getClassLoader().getResourceAsStream(INDEPENDENT_PROPERTIES_FILE)) {
+                validateInputStream(inputStream);
+                properties.load(inputStream);
+            } catch (IOException e) {
+                throw new RuntimeException("Error during loading properties from " + INDEPENDENT_PROPERTIES_FILE, e);
+            }
+        } else if (env.equals("PROD")) {
+            URI databaseUri;
+            String databaseUrlVariable = System.getenv("DATABASE_URL");
+            String portVariable = System.getenv("PORT");
+            try {
+                databaseUri = new URI(databaseUrlVariable);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException("Error during initializing databaseUri with URI from value " +
+                        "of environmental variable \"DATABASE_URL\"" + databaseUrlVariable, e);
+            }
+            properties.setProperty("jdbc.user", databaseUri.getUserInfo().split(":")[0]);
+            properties.setProperty("jdbc.password", databaseUri.getUserInfo().split(":")[1]);
+            properties.setProperty("jdbc.url", "jdbc:postgresql://" +
+                    databaseUri.getHost() + ":" +
+                    databaseUri.getPort() +
+                    databaseUri.getPath());
+            properties.setProperty("appPort", portVariable);
+        }
+    }
+
+    public String getProperty(String propertyName) {
+        return properties.getProperty(propertyName);
+    }
+
+    private void validateInputStream(InputStream inputStream) {
+        if (inputStream == null) {
+            throw new RuntimeException(".properties file not found, input stream is null");
+        }
+    }
 }
