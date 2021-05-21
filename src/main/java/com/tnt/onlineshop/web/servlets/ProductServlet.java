f27ec3cd8cdb25730cb0e877787ecbbf5d82fe41
TarasOrlovskyi <taras.orlovskyi@gmail.com>
2021-05-21 11:33:09 +0300

    Added empty ProductServlet
    1. Added empty ProductServlet

---
+++ src/main/java/com/tnt/onlineshop/web/servlets/ProductServlet.java
@@ -0,0 +1,13 @@
+package com.tnt.onlineshop.web.servlets;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ProductServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
