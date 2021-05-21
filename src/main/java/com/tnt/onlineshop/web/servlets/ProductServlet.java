1c3484d52291dd30329b998eca4c290233ee5ab9
Greengrer <sapsanoviyzmey@gmail.com>
2021-05-21 15:24:31 +0300

    local changes to pom and servlet

--- src/main/java/com/tnt/onlineshop/web/servlets/ProductServlet.java
+++ src/main/java/com/tnt/onlineshop/web/servlets/ProductServlet.java
@@ -1,10 +1,13 @@
 package com.tnt.onlineshop.web.servlets;
 
 import com.tnt.onlineshop.service.ProductService;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
@@ -22,7 +25,8 @@ public class ProductServlet extends HttpServlet {
     }
 
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        BufferedReader reader = request.getReader();
 
     }
 
