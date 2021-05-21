c410968531ecaf486c0c1599bbf445c6f3806294
TarasOrlovskyi <taras.orlovskyi@gmail.com>
2021-05-21 15:21:29 +0300

    Created doGet & doPost methods
    1. Created doGet & doPost methods

--- src/main/java/com/tnt/onlineshop/web/servlets/ProductServlet.java
+++ src/main/java/com/tnt/onlineshop/web/servlets/ProductServlet.java
@@ -1,15 +1,18 @@
 package com.tnt.onlineshop.web.servlets;
 
+import com.tnt.onlineshop.entity.Product;
+import com.tnt.onlineshop.json.JsonConverter;
 import com.tnt.onlineshop.service.ProductService;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.io.IOException;
+import java.util.List;
 
 public class ProductServlet extends HttpServlet {
 
+    private final JsonConverter jsonConverter = new JsonConverter();
     private final ProductService productService;
 
     public ProductServlet(ProductService productService) {
@@ -17,13 +20,16 @@ public class ProductServlet extends HttpServlet {
     }
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
-
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        List<Product> productList = productService.findAll();
+        String jsonProducts = jsonConverter.toJson(productList);
+        response.getWriter().write(jsonProducts);
     }
 
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) {
-
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Product product = jsonConverter.toProduct(request.getReader());
+        productService.add(product);
     }
 
     @Override
